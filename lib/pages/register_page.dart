a32bcfa6683f1e980ee67bf3a0b32d66fc41a8ac
Meegi <manuelmiggy@gmail.com>
2024-04-26 06:25:57 +0800

    can input fields as well yay, finding way to create a back button....

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -17,154 +17,157 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        decoration: BoxDecoration(
-            image: DecorationImage(image: AssetImage('assets/background.png'))),
-        child: Padding(
-          padding: EdgeInsets.only(top: 80.0),
-          child: Column(
-            children: [
-              Image(
-                image: AssetImage(
-                  'assets/terrasoi_logo.jpg',
+      body: SingleChildScrollView(
+        child: Container(
+          // width: ,
+          // height: double.infinity,
+          decoration: BoxDecoration(
+              image: DecorationImage(image: AssetImage('assets/background.png'))),
+          child: Padding(
+            padding: EdgeInsets.only(top: 80.0),
+            child: Column(
+              children: [
+                Image(
+                  image: AssetImage(
+                    'assets/terrasoi_logo.jpg',
+                  ),
+                  height: 250.0,
                 ),
-                height: 250.0,
-              ),
-              Text(
-                "Create New \nAccount",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    height: 1.1,
-                    fontFamily: "PlayfairDisplay",
-                    fontWeight: FontWeight.w600,
-                    fontSize: 45.0,
-                    color: Color.fromRGBO(4, 50, 28, 1)),
-              ),
-              SizedBox(
-                height: 30.0,
-              ),
-              Column(
-                children: [
-                  Row(
-                    children: [
-                      const SizedBox(
-                        width: 25.0,
-                      ),
-                      Image.asset(
-                        'assets/icons/farmer_icon.png',
-                        width: 40,
-                      ),
-                      const SizedBox(
-                        width: 15.0,
-                      ),
-                      SizedBox(
-                        width: 250.0,
-                        child: DropdownButtonFormField<String>(
-                          borderRadius: BorderRadius.circular(25),
-                          isDense: true,
-                          padding: EdgeInsets.all(0),
-                          decoration: InputDecoration(
-                            focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.black)),
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.black),
-                            ),
-                          ),
-                          items: status
-                              .map<DropdownMenuItem<String>>((String value) {
-                            return DropdownMenuItem<String>(
-                              value: value,
-                              child: Text(value),
-                            );
-                          }).toList(),
-                          onChanged: (String? value) {},
+                Text(
+                  "Create New \nAccount",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      height: 1.1,
+                      fontFamily: "PlayfairDisplay",
+                      fontWeight: FontWeight.w600,
+                      fontSize: 45.0,
+                      color: Color.fromRGBO(4, 50, 28, 1)),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        const SizedBox(
+                          width: 25.0,
                         ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 35.0,
-                  ),
-                  Row(
-                    children: [
-                      const SizedBox(
-                        width: 25.0,
-                      ),
-                      Image.asset(
-                        'assets/icons/mail_icon.png',
-                        width: 40,
-                      ),
-                      const SizedBox(
-                        width: 15.0,
-                      ),
-                      Container(
-                        width: 250.0,
-                        child: TextField(
-                          textAlign: TextAlign.center,
-                          decoration: InputDecoration(
-                            contentPadding: const EdgeInsets.all(0),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(25),
+                        Image.asset(
+                          'assets/icons/farmer_icon.png',
+                          width: 40,
+                        ),
+                        const SizedBox(
+                          width: 15.0,
+                        ),
+                        SizedBox(
+                          width: 250.0,
+                          child: DropdownButtonFormField<String>(
+                            borderRadius: BorderRadius.circular(25),
+                            isDense: true,
+                            padding: EdgeInsets.all(0),
+                            decoration: InputDecoration(
+                              focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.black)),
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.black),
+                              ),
                             ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(color: Colors.black),
-                              borderRadius: BorderRadius.circular(25),
-                            ),
-                            hintText: 'Email Address',
+                            items: status
+                                .map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                            onChanged: (String? value) {},
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 35.0,
-              ),
-              Row(
-                children: [
-                  const SizedBox(
-                    width: 25.0,
-                  ),
-                  Image.asset(
-                    'assets/icons/phone_icon.png',
-                    width: 40,
-                  ),
-                  const SizedBox(
-                    width: 15.0,
-                  ),
-                  Container(
-                    width: 250.0,
-                    child: TextField(
-                      textAlign: TextAlign.center,
-                      decoration: InputDecoration(
-                        contentPadding: const EdgeInsets.all(0),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(25),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 35.0,
+                    ),
+                    Row(
+                      children: [
+                        const SizedBox(
+                          width: 25.0,
                         ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(color: Colors.black),
-                          borderRadius: BorderRadius.circular(25),
+                        Image.asset(
+                          'assets/icons/mail_icon.png',
+                          width: 40,
                         ),
-                        hintText: 'Phone Number',
+                        const SizedBox(
+                          width: 15.0,
+                        ),
+                        Container(
+                          width: 250.0,
+                          child: TextField(
+                            textAlign: TextAlign.center,
+                            decoration: InputDecoration(
+                              contentPadding: const EdgeInsets.all(0),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(25),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(color: Colors.black),
+                                borderRadius: BorderRadius.circular(25),
+                              ),
+                              hintText: 'Email Address',
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 35.0,
+                ),
+                Row(
+                  children: [
+                    const SizedBox(
+                      width: 25.0,
+                    ),
+                    Image.asset(
+                      'assets/icons/phone_icon.png',
+                      width: 40,
+                    ),
+                    const SizedBox(
+                      width: 15.0,
+                    ),
+                    Container(
+                      width: 250.0,
+                      child: TextField(
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          contentPadding: const EdgeInsets.all(0),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(color: Colors.black),
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          hintText: 'Phone Number',
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 25,),
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.symmetric(horizontal: 65.0),
-                    backgroundColor: const Color.fromRGBO(155, 231, 157, 1),
-                    foregroundColor: Colors.black,
-                  ),
-                  onPressed: () {
-                  },
-                  child: const Text('Register')),
-            ],
+                  ],
+                ),
+                SizedBox(height: 25,),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(horizontal: 65.0),
+                      backgroundColor: const Color.fromRGBO(155, 231, 157, 1),
+                      foregroundColor: Colors.black,
+                    ),
+                    onPressed: () {
+                    },
+                    child: const Text('Register')),
+              SizedBox(height: 130,),
+              ],
+            ),
           ),
         ),
       ),
