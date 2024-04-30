3d33cf2675e9570d52caa5530f28279fe09febba
Meegi <manuelmiggy@gmail.com>
2024-04-30 22:28:35 +0800

    added basic home functions like appBar and bottomNavBar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,8 +11,82 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Text('test'),
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.green),
+        title: Text(
+          'Home',
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.notifications_none_outlined,
+                size: 40,
+                color: Colors.green,
+              ))
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            // SEARCH ALL
+            Row(
+              children: [],
+            )
+
+            // ANALYTICS
+            // ANALYTICS BUT COLUMN
+
+            // PENDING APPROVAL
+            // PENDING APPROVAl BUT SLIDING??? (IDK PA)
+
+            // RECENT CONTACTS
+            // SAME SHIT W BEFORE
+          ],
+        ),
+      ),
+      drawer: Drawer(),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home_outlined,
+                size: 45,
+                color: Colors.green,
+              ),
+              label: ''),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.calendar_month_outlined,
+                size: 45,
+                color: Colors.green,
+              ),
+              label: ''),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.auto_graph,
+                size: 45,
+                color: Colors.green,
+              ),
+              label: ''),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.book_outlined,
+                size: 45,
+                color: Colors.green,
+              ),
+              label: ''),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.chat_outlined,
+                size: 45,
+                color: Colors.green,
+              ),
+              label: ''),
+        ],
       ),
     );
   }
