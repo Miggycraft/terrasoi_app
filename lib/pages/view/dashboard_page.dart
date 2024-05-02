5a47ba3fa39828b1350d426d0bdd5abea8f8b0be
Meegi <manuelmiggy@gmail.com>
2024-05-03 00:59:49 +0800

    homepage done

--- lib/pages/view/dashboard_page.dart
+++ lib/pages/view/dashboard_page.dart
@@ -39,7 +39,9 @@ class _DashboardPageState extends State<DashboardPage> {
                   ),
                 ),
               ),
-              SizedBox(width: 15,),
+              SizedBox(
+                width: 15,
+              ),
               Container(
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(15),
@@ -54,6 +56,263 @@ class _DashboardPageState extends State<DashboardPage> {
               ),
             ],
           ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Analytics',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'View All',
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            decoration: BoxDecoration(
+                color: Colors.greenAccent[100],
+                borderRadius: BorderRadius.circular(15)),
+            child: Row(
+              children: [
+                Container(
+                    decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(12)),
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                    // color: Colors.green,
+                    child: Icon(
+                      Icons.people_alt,
+                      color: Colors.white,
+                      size: 35,
+                    )),
+                SizedBox(
+                  width: 12,
+                ),
+                Text(
+                  'Request Queue',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            decoration: BoxDecoration(
+                color: Colors.blueAccent[100],
+                borderRadius: BorderRadius.circular(15)),
+            child: Row(
+              children: [
+                Container(
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(12)),
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                    // color: Colors.green,
+                    child: Icon(
+                      Icons.how_to_vote,
+                      color: Colors.white,
+                      size: 35,
+                    )),
+                SizedBox(
+                  width: 12,
+                ),
+                Text(
+                  'In Approval',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            decoration: BoxDecoration(
+                color: Colors.orangeAccent[100],
+                borderRadius: BorderRadius.circular(15)),
+            child: Row(
+              children: [
+                Container(
+                    decoration: BoxDecoration(
+                        color: Colors.orange,
+                        borderRadius: BorderRadius.circular(12)),
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                    // color: Colors.green,
+                    child: Icon(
+                      Icons.border_color,
+                      color: Colors.white,
+                      size: 35,
+                    )),
+                SizedBox(
+                  width: 12,
+                ),
+                Text(
+                  'In Signature',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            decoration: BoxDecoration(
+                color: Colors.pinkAccent[100],
+                borderRadius: BorderRadius.circular(15)),
+            child: Row(
+              children: [
+                Container(
+                    decoration: BoxDecoration(
+                        color: Colors.pink,
+                        borderRadius: BorderRadius.circular(12)),
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                    // color: Colors.green,
+                    child: Icon(
+                      Icons.edit_note,
+                      color: Colors.white,
+                      size: 35,
+                    )),
+                SizedBox(
+                  width: 12,
+                ),
+                Text(
+                  'In Draft',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Pending Approval',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'View All',
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 60, vertical: 15),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(color: Colors.orange, width: 2),
+                ),
+                child: Text(
+                  'SHIVA',
+                  style: TextStyle(
+                      color: Colors.orange, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 60, vertical: 15),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(color: Colors.orange, width: 2),
+                ),
+                child: Text(
+                  'SHIVA',
+                  style: TextStyle(
+                      color: Colors.orange, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 8,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Recent Contacts',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'View All',
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 60, vertical: 15),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(color: Colors.lightGreen, width: 2),
+                ),
+                child: Text(
+                  'ARJO',
+                  style: TextStyle(
+                      color: Colors.lightGreen, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 60, vertical: 15),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(color: Colors.lightGreen, width: 2),
+                ),
+                child: Text(
+                  'MARIE',
+                  style: TextStyle(
+                      color: Colors.lightGreen, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 2,),
+
         ],
       ),
     );
