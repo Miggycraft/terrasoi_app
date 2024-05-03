36c9fbf5b686f772c2e2fb7920cf1e760592cad6
Sinichi2 <110707459+Sinichi2@users.noreply.github.com>
2024-05-03 15:21:29 +0800

    damn

--- lib/pages/RPages/TUGtF.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
-
-// class TUGtF extends StatefulWidget {
-//   const TUGtF({super.key});
-//   @override
-//   State<TUGtF> createState() => _TUGtFState();
-// }
--- lib/pages/view/scheduling_page.dart
+++ lib/pages/view/scheduling_page.dart
@@ -12,13 +12,19 @@ class _SchedulingPageState extends State<SchedulingPage> {
   late DateTime _selectedDay;
   late DateTime _focusedDay;
   late final Map<DateTime, List<Map<String, String>>> _selectedEvents;
+  // Constants for month and year selection
+  final List<String> months = List<String>.generate(12, (index) => '${index + 1}');
+  final int currentYear = DateTime.now().year;
+  final List<String> years = List<String>.generate(21, (index) => '${currentYear - 10 + index}');
+
+  String? selectedMonth;
+  String? selectedYear;
 
   @override
   void initState() {
     super.initState();
     _selectedDay = DateTime.now();
     _focusedDay = DateTime.now();
-    // Initializing event details for the current week's Monday
     _selectedEvents = {
       DateTime.now().subtract(Duration(days: DateTime.now().weekday - 1)): [
         {'time': '08:00 - 09:00 AM', 'event': 'Packaging'},
@@ -27,6 +33,8 @@ class _SchedulingPageState extends State<SchedulingPage> {
         {'time': '11:00 - 12:00 AM', 'event': 'Arrival'},
       ],
     };
+    selectedMonth = '${_focusedDay.month}';
+    selectedYear = '${_focusedDay.year}';
   }
 
   List<Map<String, String>> _getEventsForDay(DateTime day) {
@@ -36,48 +44,85 @@ class _SchedulingPageState extends State<SchedulingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+        body: Column(
         children: [
-          TableCalendar(
-            firstDay: DateTime.utc(2020, 1, 1),
-            lastDay: DateTime.utc(2030, 12, 31),
-            focusedDay: _focusedDay,
-            calendarFormat: CalendarFormat.week,
-            startingDayOfWeek: StartingDayOfWeek.sunday,
-            selectedDayPredicate: (day) {
-              return isSameDay(_selectedDay, day);
-            },
-            onDaySelected: (selectedDay, focusedDay) {
-              setState(() {
-                _selectedDay = selectedDay;
-                _focusedDay = focusedDay;
-              });
-            },
-            onPageChanged: (focusedDay) {
-              _focusedDay = focusedDay;
-            },
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: _getEventsForDay(_selectedDay).length,
-              itemBuilder: (context, index) {
-                final event = _getEventsForDay(_selectedDay)[index];
-                return Card(
-                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      backgroundColor: Colors.green,
-                      child: Icon(Icons.check, color: Colors.white),
-                    ),
-                    title: Text(event['event'] ?? 'No event'),
-                    subtitle: Text('${_selectedDay.month}/${_selectedDay.day}/${_selectedDay.year} | ${event['time']}'),
-                  ),
-                );
-              },
-            ),
-          ),
+        Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        DropdownButton<String>(
+        value: selectedMonth,
+        items: months.map<DropdownMenuItem<String>>((String value) {
+      return DropdownMenuItem<String>(
+        value: value,
+        child: Text(value),
+      );
+    }).toList(),
+    onChanged: (value) {
+    setState(() {
+    selectedMonth = value;
+    _focusedDay = DateTime(int.parse(selectedYear!), int.parse(selectedMonth!), _focusedDay.day);
+    });
+    },
+    ),
+    DropdownButton<String>(
+    value: selectedYear,
+    items: years.map<DropdownMenuItem<String>>((String value) {
+    return DropdownMenuItem<String>(
+    value: value,
+    child: Text(value),
+    );
+    }).toList(),
+    onChanged: (value) {
+    setState(() {
+    selectedYear = value;
+    _focusedDay = DateTime(int.parse(selectedYear!), int.parse(selectedMonth!), _focusedDay.day);
+    });
+    },
+    ),
+    ],
+    ),
+    TableCalendar(
+    firstDay: DateTime.utc(2020, 1, 1),
+    lastDay: DateTime.utc(2030, 12, 31),
+    focusedDay: _focusedDay,
+    calendarFormat: CalendarFormat.month,
+    startingDayOfWeek: StartingDayOfWeek.sunday,
+    selectedDayPredicate: (day) {
+    return isSameDay(_selectedDay, day);
+    },
+    onDaySelected: (selectedDay, focusedDay) {
+    setState(() {
+    _selectedDay = selectedDay;
+    _focusedDay = focusedDay;
+    });
+    },
+    onPageChanged: (focusedDay) {
+    _focusedDay = focusedDay;
+    },
+    ),
+    Expanded(
+    child: ListView.builder(
+    itemCount: _getEventsForDay(_selectedDay).length,
+    itemBuilder: (context, index) {
+    final event = _getEventsForDay(_selectedDay)[index];
+    return Card(
+    margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+    child: ListTile(
+    leading: CircleAvatar(
+    backgroundColor: Colors.green,
+    child: Icon(Icons.check, color: Colors.white),
+    ),
+      title: Text(event['event'] ?? 'No event'),
+      subtitle: Text('${_selectedDay.month}/${_selectedDay.day}/${_selectedDay.year} | ${event['time']}'),
+    ),
+    );
+    },
+    ),
+    ),
         ],
-      ),
+        ),
     );
   }
 }
+
+
