c91c00ab510b5c2f0eefb3ede6665655225897cd
Sinichi2 <110707459+Sinichi2@users.noreply.github.com>
2024-05-03 11:11:01 +0800

    Gonna be changing
    LMAO

--- lib/pages/view/resources_page.dart
+++ lib/pages/view/resources_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class ResourcesPage extends StatefulWidget {
   const ResourcesPage({super.key});
@@ -10,9 +11,60 @@ class ResourcesPage extends StatefulWidget {
 class _ResourcesPageState extends State<ResourcesPage> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: EdgeInsets.symmetric(horizontal:30.0),
-        child: Text('Resources...'),);
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start, // Align text to the left
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Weekly Article 04/04/2024',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0), // Rounded corners for the card
+              ),
+              margin: EdgeInsets.all(16),
+              child: ClipRRect( // Clip the image with rounded corners
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(15.0),
+                  topRight: Radius.circular(15.0),
+                ),
+                child: Image.asset(
+                  'assets/Pictures/FarmBlog2.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Column(
+                children: [
+                  ListTile(
+                    title: Text(
+                      'The Ultimate Guide to Farming',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'Read a selection of opinion pieces from world class journalists only on our blog',
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Text(
+                      'Mr. Nice Guy | 15 min read',
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // If you have more content or elements to add, continue here
+          ],
+        ),
+      ),
+    );
   }
-
 }
